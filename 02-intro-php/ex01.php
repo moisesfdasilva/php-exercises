8547bef86bfaf55dfa104f64e492650dd67d5293
moisesfdasilva <msesfernandes@gmail.com>
2024-04-05 21:42:16 -0300

    feat: create ex01.php in 02-into.

---
+++ 02-intro-php/ex01.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <body>
+    <?php
+      echo("<ul>");
+      for($i = 1; $i <= 100; $i++){
+        echo("<li>".$i."</li>");
+      }
+      echo("</ul>");
+    ?>
+  </body>
+</html>
