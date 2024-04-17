577f7be950b957df3cf96bdd616a6bad931811ac
moisesfdasilva <msesfernandes@gmail.com>
2024-04-16 23:38:08 -0300

    feat: create style in all pages of 05-website-empresa.

---
+++ 05-website-empresa/paginadeprodutos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Website de Empresa</title>
+  </head>
+  <body>
+    <header class="header">
+      <img class="logo" src="imagens/logo1.png">
+      <nav class="nav-top">
+        <ul>
+          <li><a href="paginaprincipal.html">Principal</a></li>
+          <li><a href="paginasobre.html">Sobre</a></li>
+          <li><a href="paginadeprodutos.php">Produtos</a></li>
+          <li><a href="paginadenovidades.php">Novidades</a></li>
+          <li><a href="paginadecontato.php">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="main">
+      <section class="main-section">
+        <article class="article">
+        </article>
+      </section>
+    </main>
+    <footer class="footer">
+      <p>Av. Olegário Maciel, 777 - Loja A - Barra da Tijuca</p>
+      <p>CEP: 22621-200 | CNPJ: 12.585.556/0001-25</p>
+    </footer>
+  </body>
+</html>
